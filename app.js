79eb4db3f3b66d7f1db21f888169f468376b2b4a
ShymaaRagab <102189654+ShymaaRagab@users.noreply.github.com>
2023-10-18 00:06:40 +0300

    Task Done with 2 solution

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-let icon1 = document.getElementById("icon1"),
-  icon2 = document.getElementById("icon2"),
-  icon3 = document.getElementById("icon3"),
-  icon4 = document.getElementById("icon4"),
-  icon5 = document.getElementById("icon5");
-
-icon1.addEventListener("click", () => {
-  copyCode("c1");
-});
-icon2.addEventListener("click", () => {
-  copyCode("c2");
-});
-icon3.addEventListener("click", () => {
-  copyCode("c3");
-});
-icon4.addEventListener("click", () => {
-  copyCode("c4");
-});
-icon5.addEventListener("click", () => {
-  copyCode("c5");
+//solution 1
+let icons = document.querySelectorAll("i");
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    copyCode(document.querySelector(`#${icon.id}~code`).id);
+  });
 });
 
-// id => is for each code
+//you can use this code instead above
+/*let icon1 = document.getElementById("icon1"),
+    icon2 = document.getElementById("icon2"),
+   icon3 = document.getElementById("icon3"),
+   icon4 = document.getElementById("icon4"),
+   icon5 = document.getElementById("icon5");
+ icon1.addEventListener("click", () => {
+   copyCode("c1");
+ });
+ icon2.addEventListener("click", () => {
+   copyCode("c2");
+ });
+ icon3.addEventListener("click", () => {
+   copyCode("c3");
+ });
+ icon4.addEventListener("click", () => {
+   copyCode("c4");
+ });
+ icon5.addEventListener("click", () => {
+   copyCode("c5");
+});*/
+
+// id => is for each "<code></code>"
 function copyCode(id) {
   let targetText = document.getElementById(id).textContent;
   navigator.clipboard
